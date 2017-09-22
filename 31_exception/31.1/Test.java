cd220cbe5c3cbdf4431469ecffe05f68f6a4bbef
ph <944797590@qq.com>
2017-09-22 12:04:50 +0800

    0c, throws throw

---
+++ 31_exception/31.1/Test.java
@@ -0,0 +1,11 @@
+class Test {
+	public static void main(String args []) {
+		User user = new User();
+		try {
+			user.setAge(-20);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
