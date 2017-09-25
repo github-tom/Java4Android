9bc37e899fcbd7633b0760aaff7c530e846ae807
ph <944797590@qq.com>
2017-09-25 17:08:55 +0800

    12, array

---
+++ 40_array/Test.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main(String args []) {
+        //数组的静态申明
+        //int arr[] = {5,2,7,9,0,90,20};
+        //数组的动态申明
+        int arr[] = new int[10];
+        char arrChar[] = new char[10];
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
